79baf11e060053c2f0bd6c1d59fdda0676a7bce3
Lior Maimon <lmaimon20@gmail.com>
2022-11-17 17:21:56 +0200

    fourth

--- LiorMaimon.java
+++ LiorMaimon.java
@@ -1,7 +1,7 @@
 public class LiorMaimon {
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Lior Maimon" +i);
+			System.out.println("Lior Maimon " +i);
 		}
 	}
 }
